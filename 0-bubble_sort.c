7913d723ab25668a04f28b8951478e781170eb58
jovaniemk <jovaniemk@gmail.com>
2024-03-03 06:31:49 -0800

    using bubble_sort and selection_sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - is a function that uses bubble
+ * sorting algorithms to sort an arrays
+ * @array: pointer to an array
+ * @size: size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				int temp;
+
+				temp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
